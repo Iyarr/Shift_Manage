d6206eb2fb96869608b8cfed47e5f2afd2295557
Iyarr <8.24ridiculous@gmail.com>
2023-06-14 14:51:56 +0900

    コマごとの分割

--- front/app-frontend/src/body/shift/calender.tsx
+++ front/app-frontend/src/body/shift/calender.tsx
@@ -1,50 +1,63 @@
-import { useState,useRef,useEffect } from 'react';
-import { Calendar,ToolbarInput,CustomButtonInput } from '@fullcalendar/core'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import { useState, useRef, useEffect } from "react";
+import { Calendar, ToolbarInput, CustomButtonInput } from "@fullcalendar/core";
+import { SlotLabelContentArg } from "@fullcalendar/common";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import internal from "stream";
 
-declare module '@fullcalendar/core' {
-  interface CalendarOptions {
-    // カレンダーオプションの型定義を追加
-    // 例: plugins?: PluginDef[] | PluginDef;
-  }
-}
 function CalenderComponent() {
-    const views = ["dayGridWeek","dayGridMonth"]
-    const [count, setCount] = useState<number>(0)
-    const calendarRef = useRef<FullCalendar>(null);
+  //const views = ["dayGridWeek", "dayGridMonth"];
+  //const [count, setCount] = useState<number>(0);
+  const calendarRef = useRef<FullCalendar>(null);
+  const partition = ["X", "Y", "Z", "A", "B", "C", "D"];
 
-    const calendarOptions = {
-        ref:calendarRef,
-        plugins:[ dayGridPlugin, interactionPlugin ],
-        initialView:views[count],
-        locale:"ja",
-        hiddenDays:[0],
-        dayCellContent:() => {},
-        headerToolbar:{
-            start:"title",
-            center: 'today prev,next',
-            end: 'myCustomButton'
+  const calendarOptions = {
+    ref: calendarRef,
+    plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+    initialView: "timeGridWeek",
+    allDaySlot: false,
+    slotMinTime: "0:00",
+    slotMaxTime: "7:00",
+    slotDuration: "1:00",
+    slotLabelContent: (arg: SlotLabelContentArg) => {
+      const num = Number(arg.text[0]);
+      return partition[num];
+    },
+
+    selectable: true,
+    locale: "ja",
+    hiddenDays: [0],
+    headerToolbar: {
+      start: "title",
+      center: "today prev,next",
+      end: "",
+      //end: "myCustomButton"
+    },
+    /* 一旦機能は廃止
+    customButtons: {
+      myCustomButton: {
+        text: "表示切替",
+        click: () => {
+          if (calendarRef.current) {
+            calendarRef.current.getApi().changeView(views[count]);
+            setCount((count + 1) % 2);
+          }
         },
-        customButtons:{
-            myCustomButton:{
-                text: '表示切替',
-                click: () => {
-                    if (calendarRef.current) {
-                        calendarRef.current.getApi().changeView(views[count]);
-                        setCount((count+1)%2)
-                    }
-                }
-            }
-        }
-    }
-    return(
-      <div className="calender">
-        <FullCalendar {...calendarOptions}/>
-      </div>
-    );
+      },
+    },
+    */
+  };
+  return (
+    <div className="calender">
+      <FullCalendar {...calendarOptions} />
+    </div>
+  );
 }
 export default CalenderComponent;
 
-
+function DayCellContent() {
+  return;
+}
